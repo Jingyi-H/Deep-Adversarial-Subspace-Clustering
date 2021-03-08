f1906040c04ca954f92514d31552d99d07678725
Tina Hwang <tina_thedreamer@outlook.com>
2021-03-08 16:19:53 +0800

    fix bugs in loss.py

--- loss.py
+++ loss.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tensorflow.keras import backend as K
+
 
 def projection_residual(z):
 	pass
@@ -6,9 +8,13 @@ def projection_residual(z):
 def reconst_loss(x_true, x_reconst):
 	return tf.reduce_mean(tf.square(x_true - x_reconst))
 
-def ae_loss(x_true, x_reconst, z_conv, z_se, theta, lambda1=1, lambda2=1, lambda3=1):
-	reconst_loss = tf.reduce_mean(tf.square(x_true - x_reconst))
-	self_expr_loss = tf.reduce_mean(tf.square(z_se - z_conv))
-	penalty = (tf.norm(theta))^2
+def ae_loss(x_true, x_reconst, z_conv, z_se, theta, lambda1=0.5, lambda2=15, lambda3=1):
+	x_reconst = tf.cast(x_reconst, dtype=tf.float64)
+	reconst_loss = tf.reduce_sum(tf.square(x_true - x_reconst))
+	self_expr_loss = tf.cast(tf.reduce_sum(tf.square(z_se - z_conv)), dtype=tf.float64)
+	norm = tf.norm(theta, keepdims=True)
 
-	return lambda1 * reconst_loss + lambda2 * self_expr_loss + lambda3 * penalty
+	penalty = tf.cast(tf.matmul(norm, norm), dtype=tf.float64)
+	# print(reconst_loss.dtype, self_expr_loss.dtype, penalty.dtype)
+	loss = lambda1 * reconst_loss + lambda2 * self_expr_loss + lambda3 * penalty
+	return [loss, reconst_loss, self_expr_loss, penalty]
