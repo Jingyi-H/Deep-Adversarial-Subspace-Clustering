6cd4ef28643a2cda2ae20a54b909a002ed0dd645
Tina Hwang <tina_thedreamer@outlook.com>
2021-03-24 22:54:06 +0800

    update loss

--- loss.py
+++ loss.py
@@ -2,13 +2,18 @@ import tensorflow as tf
 from tensorflow.keras import backend as K
 
 
-def projection_residual(z, proj):
+def projection_residual(z, proj, flag=True):
 	# z = tf.cast(z, dtype='float64')
 	# proj = tf.cast(proj, dtype='float64')
-	loss = tf.norm(z - proj, keepdims=True)
-	# loss = tf.reshape(loss, [1, 1])
-	loss = tf.matmul(loss, loss)
-	# loss = tf.cast(tf.matmul(loss, loss), dtype=tf.float64)
+	if flag == True:
+		loss = tf.norm(z - proj, keepdims=True)
+		# loss = tf.reshape(loss, [1, 1])
+		loss = tf.matmul(loss, loss)
+		# loss = tf.cast(tf.matmul(loss, loss), dtype=tf.float64)
+	else:
+		loss = tf.norm(z - proj, axis=0)
+		loss = loss * loss
+		# print(loss.shape)
 
 	return loss
 
@@ -16,33 +21,53 @@ def reconst_loss(x_true, x_reconst):
 	return tf.reduce_mean(tf.square(x_true - x_reconst))
 
 def ae_loss(x_true, x_reconst, z_conv, z_se, theta, lambda1=0.5, lambda2=15, lambda3=1):
-	x_reconst = tf.cast(x_reconst, dtype=tf.float64)
+	# x_reconst = tf.cast(x_reconst, dtype=tf.float64)
+	x_true = tf.cast(x_true, dtype=x_reconst.dtype)
 	reconst_loss = tf.reduce_sum(tf.square(x_true - x_reconst))
-	self_expr_loss = tf.cast(tf.reduce_sum(tf.square(z_se - z_conv)), dtype=tf.float64)
+	self_expr_loss = tf.reduce_sum(tf.square(z_se - z_conv))
+	# self_expr_loss = tf.cast(tf.reduce_sum(tf.square(z_se - z_conv)), dtype=tf.float64)
 	norm = tf.norm(theta, keepdims=True)
 
-	penalty = tf.cast(tf.matmul(norm, norm), dtype=tf.float64)
+	penalty = tf.matmul(norm, norm)
+	# penalty = tf.cast(tf.matmul(norm, norm), dtype=tf.float64)
 	# print(reconst_loss.dtype, self_expr_loss.dtype, penalty.dtype)
 	loss = lambda1 * reconst_loss + lambda2 * self_expr_loss + lambda3 * penalty
 
 	return [loss, reconst_loss, self_expr_loss, penalty]
 
-def L_D(z, proj, kcluster, epsilon=0):
+def L_r(z, proj, kcluster):
 	loss = 0
 	for k in range(kcluster):
-		m = z[k].shape[1]										# z[k] 列向量构成的矩阵，m为向量数即样本数
-		loss = loss + projection_residual(z[k], proj[k])/m		# + max(epsilon - projection_residual(z[k], proj[k]), 0)/m
+		# m = z[k].shape[1]										# z[k] 列向量构成的矩阵，m为向量数即样本数
+		loss = loss + tf.reduce_mean(projection_residual(z[k], proj[k], flag=False))		# + max(epsilon - projection_residual(z[k], proj[k]), 0)/m
 
 	loss = loss/kcluster
 
 	return loss
 
-def L_a(_z, _U, kcluster):
+def L_D(real_z, fake_z, real_proj, fake_proj, kcluster, epsilon=0.1):
+	# loss = L_r(real_z, real_proj, kcluster) + max(0, epsilon/kcluster - L_r(fake_z, fake_proj, kcluster))
 	loss = 0
 	for k in range(kcluster):
-		m = z[k].shape[1]			# z[k] 列向量构成的矩阵，m为向量数即样本数
-		loss = loss + projection_residual(_z[k], _U[k])/m
-
-	loss = loss/kcluster
+		m_ = fake_z[k].shape[1]
+		z = real_z[k][:, 0:m_]
+		term2 = epsilon * tf.ones([1, m_], dtype=z.dtype) - projection_residual(fake_z[k], fake_proj[k], flag=False)
+		loss = loss + projection_residual(z, real_proj[k][:, 0:m_])/m_ + tf.reduce_mean(tf.maximum(term2, tf.zeros([1, m_], dtype=z.dtype)))
 
 	return loss
+
+def r1():
+	pass
+
+def r2():
+	pass
+
+# def L_a(z, proj, kcluster):
+# 	loss = 0
+# 	for k in range(kcluster):
+# 		m = z[k].shape[1]			# z[k] 列向量构成的矩阵，m为向量数即样本数
+# 		loss = loss + projection_residual(_z[k], _U[k])/m
+#
+# 	loss = loss/kcluster
+#
+# 	return loss
