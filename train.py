30ab9dfaa7e4e6ad1aef8cc2857c7d8bc879005f
Tina Hwang <tina_thedreamer@outlook.com>
2021-03-25 18:01:32 +0800

    modify optimizer: add learning rate

--- train.py
+++ train.py
@@ -5,31 +5,35 @@ import tensorflow as tf
 import os
 import h5py
 import scipy.io as sio
-from sklearn.cluster import SpectralClustering
+
 import loss
 from model import ConvAE
 from model import DASC
 import utils
 
 # os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import metric
+
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-def train(train_data, batch_size=72, input_shape=[32,32,1], epoch_num=10, k=20, d_iter_num=5, r=30, save_dir='model'):
+def train(train_data, label, batch_size=72, input_shape=[32,32,1], epoch_num=10, pre_train_epoch=10,
+          k=20, d_iter_num=5, r=30, alpha=0.95, g_lr=1e-3, d_lr=2e-4, save_dir='model'):
 
     inputs = tuple([batch_size] + input_shape)
     print(inputs)
     dasc = DASC(input_shape=inputs, kcluster=20)
-    dasc.initialize(train_data)
+    dasc.initialize(train_data, pre_train_epoch=pre_train_epoch)
 
+    print("Start Training...")
     g_var = dasc.conv_ae.trainable_variables
     # d_var = dasc._U
-    optimizer = tf.optimizers.Adam(lr=2e-4)
-
+    d_optim = tf.optimizers.Adam(lr=d_lr)
+    g_optim = tf.optimizers.Adam(lr=g_lr)
     for epoch in range(epoch_num):
         for (batch, (x_batch, x_r_batch)) in enumerate(train_data):
             with tf.GradientTape(watch_accessed_variables=False) as gtape:
-                real_z, fake_z = dasc.G(x_batch)
-                dasc.D(real_z, fake_z, r)
+                real_z, fake_z = dasc.G(x_batch, alpha=alpha)
+                real_z = dasc.D(real_z, fake_z, r)
                 d_var = [dasc._U[i].trainable_variables[0] for i in range(k)]
                 # print(d_var)
                 for i in range(d_iter_num):
@@ -42,7 +46,7 @@ def train(train_data, batch_size=72, input_shape=[32,32,1], epoch_num=10, k=20, 
                         # print("L_d: {}".format(str(l_d)))
                     grads = tape.gradient(l_d, d_var)
                     # print(tape.watched_variables())
-                    optimizer.apply_gradients(zip(grads, d_var))
+                    d_lr.apply_gradients(zip(grads, d_var))
                 # print("Epoch[{}]: D loss={}".format(str(epoch+1), str(l_d)))
 
                 # D_loss = loss.L_D(cluster, d_var, k, dasc._m)
@@ -53,13 +57,16 @@ def train(train_data, batch_size=72, input_shape=[32,32,1], epoch_num=10, k=20, 
                 z_conv = dasc.conv_ae.z_conv
                 z_se = dasc.conv_ae.z_se
                 theta = dasc.conv_ae.layers[1].get_weights()[0]
+                theta = np.reshape(theta, [batch_size, batch_size])
                 rec_loss, reconst_loss, self_expr_loss, penalty = loss.ae_loss(x_r_batch, x_reconst, z_conv, z_se, theta)
                 G_loss = rec_loss + loss.L_r(fake_z, proj_fake, k)
+                acc = metric.get_acc(theta, label)
                 grads = gtape.gradient(G_loss, g_var)
-                optimizer.apply_gradients(zip(grads, g_var))
+                g_optim.apply_gradients(zip(grads, g_var))
 
-            print("epoch[{}]: G_loss={}\tD_loss={}\treconst_loss={}\tself_expr_loss={}\tpenalty={}".format(
-                str(epoch+1), str(float(rec_loss)), str(float(l_d)), str(reconst_loss.numpy()), str(self_expr_loss.numpy()), str(penalty.numpy().ravel()[0])))
+            print("Epoch[{}/{}]: G_loss={}\tD_loss={}\tacc={}".format(str(epoch+1), str(epoch_num), str(float(G_loss)), str(float(l_d)), str(float(acc))))
+            # print("epoch[{}]: G_loss={}\tD_loss={}\treconst_loss={}\tself_expr_loss={}\tpenalty={}".format(
+            #     str(epoch+1), str(float(rec_loss)), str(float(l_d)), str(reconst_loss.numpy()), str(self_expr_loss.numpy()), str(penalty.numpy().ravel()[0])))
 
     theta = dasc.conv_ae.layers[1].get_weights()[0]
     theta = np.reshape(theta, [batch_size, batch_size])
@@ -116,46 +123,19 @@ def train_ConvAE(train_data, batch_size=1440, input_shape=[32,32,1], epoch_num=1
     # conv_ae.save_weights('logs/conv_ae_weights')
     return _z_conv, _theta
 
-def load_data(path='data/COIL20.mat'):
-    try:
-        print("loading data from {}\{}...".format(os.getcwd(), path))
-        coil20 = sio.loadmat(path)
-        img = coil20['fea']
-        label = coil20['gnd']
-        img = np.reshape(img,(img.shape[0],32,32,1))
-        return img, label
-    except Exception as e:
-        print(e)
-        return None
+
+# img, label = load_data()
+# img, label = utils.shuffle(img, label)
+# train_db = tf.data.Dataset.from_tensor_slices((img, img))
+# train_db = train_db.batch(1440)
 
 
-
-def predict():
-    pass
-
-img, label = load_data()
-img, label = utils.shuffle(img, label)
-train_db = tf.data.Dataset.from_tensor_slices((img, img))
-train_db = train_db.batch(1440)
-
-label = label.astype(int)
-label = label.reshape([1, -1])
-print(label)
-theta = train(train_db, epoch_num=30, batch_size=1440)
+# theta = train(train_db, label, epoch_num=30, batch_size=1440)
 # np.savetxt('theta.txt', theta)
 # np.savetxt('label.txt', label)
 # theta_T = np.transpose(theta)
 
 # z, theta = train_ConvAE(train_db, epoch_num=10, batch_size=1440)
-affinity = np.abs(theta + theta.T)
-affinity = np.abs(affinity)
-sc = SpectralClustering(20, affinity='precomputed', n_init=100, assign_labels='discretize')
-pred = sc.fit_predict(affinity)
-pred = pred.astype(int)
-pred = utils.best_map(label, pred)
-pred = pred.astype(int)
 
-label = label.reshape(pred.shape)
-print(np.argwhere(pred-label==0).shape)
-# np.savetxt('pred.txt', label_map)
+# np.savetxt('pred.txt', pred)
 # z, theta = train_ConvAE(train_db, epoch_num=10, batch_size=1440)
