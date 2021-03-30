652b4e1861ed704ebb6a66d968fcb988f579be6f
Tina Hwang <tina_thedreamer@outlook.com>
2021-03-30 23:45:03 +0800

    update train.py

--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-import math
-
 import numpy as np
 import tensorflow as tf
 import os
@@ -10,18 +8,17 @@ import loss
 from model import ConvAE
 from model import DASC
 import utils
+from sklearn.cluster import SpectralClustering
 
-# os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-import metric
-
+# block warnings
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-def train(train_data, label, batch_size=72, input_shape=[32,32,1], epoch_num=10, pre_train_epoch=10,
+def train(train_data, batch_size=72, input_shape=[32,32,1], epoch_num=10, pre_train_epoch=10,
           k=20, d_iter_num=5, r=30, alpha=0.95, g_lr=1e-3, d_lr=2e-4, save_dir='model'):
 
     inputs = tuple([batch_size] + input_shape)
     print(inputs)
-    dasc = DASC(input_shape=inputs, kcluster=20)
+    dasc = DASC(ConvAE, input_shape=inputs, kcluster=20)
     dasc.initialize(train_data, pre_train_epoch=pre_train_epoch)
 
     print("Start Training...")
@@ -30,10 +27,10 @@ def train(train_data, label, batch_size=72, input_shape=[32,32,1], epoch_num=10,
     d_optim = tf.optimizers.Adam(lr=d_lr)
     g_optim = tf.optimizers.Adam(lr=g_lr)
     for epoch in range(epoch_num):
-        for (batch, (x_batch, x_r_batch)) in enumerate(train_data):
+        for (batch, (x_batch, y_batch)) in enumerate(train_data):
             with tf.GradientTape(watch_accessed_variables=False) as gtape:
                 real_z, fake_z = dasc.G(x_batch, alpha=alpha)
-                real_z = dasc.D(real_z, fake_z, r)
+                dasc.D(real_z, fake_z, r)
                 d_var = [dasc._U[i].trainable_variables[0] for i in range(k)]
                 # print(d_var)
                 for i in range(d_iter_num):
@@ -43,10 +40,9 @@ def train(train_data, label, batch_size=72, input_shape=[32,32,1], epoch_num=10,
                         fake_proj = dasc.forward(fake_z)
                         l_d = loss.L_D(real_z, fake_z, real_proj, fake_proj, k)
                         l_d = l_d + loss.r1(d_var) + loss.r2(d_var)
-                        # print("L_d: {}".format(str(l_d)))
-                    grads = tape.gradient(l_d, d_var)
-                    # print(tape.watched_variables())
-                    d_lr.apply_gradients(zip(grads, d_var))
+                        print("\tLoss_D: loss={}\tR1={}\tR2={}".format(str(float(l_d)), str(float(loss.r1(d_var))), str(float(loss.r2(d_var)))))
+                    d_grads = tape.gradient(l_d, d_var)
+                    d_optim.apply_gradients(zip(d_grads, d_var))
                 # print("Epoch[{}]: D loss={}".format(str(epoch+1), str(l_d)))
 
                 # D_loss = loss.L_D(cluster, d_var, k, dasc._m)
@@ -56,15 +52,25 @@ def train(train_data, label, batch_size=72, input_shape=[32,32,1], epoch_num=10,
                 x_reconst = dasc.conv_ae(x_batch)
                 z_conv = dasc.conv_ae.z_conv
                 z_se = dasc.conv_ae.z_se
+
                 theta = dasc.conv_ae.layers[1].get_weights()[0]
                 theta = np.reshape(theta, [batch_size, batch_size])
-                rec_loss, reconst_loss, self_expr_loss, penalty = loss.ae_loss(x_r_batch, x_reconst, z_conv, z_se, theta)
+                rec_loss, reconst_loss, self_expr_loss, penalty = loss.ae_loss(x_batch, x_reconst, z_conv, z_se, theta)
                 G_loss = rec_loss + loss.L_r(fake_z, proj_fake, k)
-                acc = metric.get_acc(theta, label)
-                grads = gtape.gradient(G_loss, g_var)
-                g_optim.apply_gradients(zip(grads, g_var))
+                # calculate accuracy of prediction
+                # theta = utils.theta_normalize(theta)
+                affinity = 0.5 * (np.abs(theta) + np.abs(theta.T))
+                sc = SpectralClustering(n_clusters=20, eigen_solver='arpack', affinity='precomputed', n_init=100, assign_labels='kmeans')
+                y_hat = sc.fit_predict(affinity)
+                y_hat = y_hat.astype(int)
+                y_true = tf.reshape(y_batch, y_hat.shape)
+                acc = utils.cluster_accuracy(y_true, y_hat)
 
-            print("Epoch[{}/{}]: G_loss={}\tD_loss={}\tacc={}".format(str(epoch+1), str(epoch_num), str(float(G_loss)), str(float(l_d)), str(float(acc))))
+                g_grads = gtape.gradient(G_loss, g_var)
+
+                g_optim.apply_gradients(zip(g_grads, g_var))
+
+            print("Epoch[{}/{}]: G_loss={}\tD_loss={}\tpenalty={}\tacc={}".format(str(epoch+1), str(epoch_num), str(float(G_loss)), str(float(l_d)), str(float(penalty)), str(float(acc))))
             # print("epoch[{}]: G_loss={}\tD_loss={}\treconst_loss={}\tself_expr_loss={}\tpenalty={}".format(
             #     str(epoch+1), str(float(rec_loss)), str(float(l_d)), str(reconst_loss.numpy()), str(self_expr_loss.numpy()), str(penalty.numpy().ravel()[0])))
 
